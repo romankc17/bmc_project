1bb5862e64164795a5406d6f6724a926c81f498b
Darshan <nepaleseboyooo@gmail.com>
2021-06-19 23:22:47 +0545

    error in forms.py

--- backend/accounts/forms.py
+++ backend/accounts/forms.py
@@ -1,21 +1,31 @@
+
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from django.forms import fields
+
+from .models import UserProfile
 
 class ExtendedUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
     
+    
     class Meta:
         model = User
-        fields = ('username', 'emali', 'password1', 'password2')
+        fields = ('emaii', 'password1', 'password2')
 
     def save(self, commit=True):
         user = super().save(commit=False) 
+        
+
+        batch = self.cleaned_data['batch']
+        roll_no = self.cleaned_data['roll_no']
+
         user.email = self.cleaned_data['email']
+        user.username = int(str(batch) + str(roll_no))
         
         if commit:
             user.save()
+            
         return user
 
 
--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,9 +1,34 @@
+from django.db import reset_queries
 from django.shortcuts import render,redirect
-from django.contrib.auth.models import User,auth
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from .forms import ExtendedUserCreationForm, UserProfileForm
 
 # Create your views here.
 def register(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+
+        form = ExtendedUserCreationForm(request.POST)
+        profile_form = UserProfileForm(request.POST)
+
+        
+        if form.is_valid():
+            user = form.save()
+
+            profile = profile_form.save(commit=False)
+            profile.user = user
+
+            profile.save()
+
+            return redirect('index')
+            
+            
+    else:
+        form = ExtendedUserCreationForm()
+        profile_form = UserProfileForm()
+
+    context = {'form':form, 'profile_form': profile_form}
+    return render(request, 'accounts/register.html')
+
+
     return render (request, 'accounts/register.html',  {'form': form})
 
