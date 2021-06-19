1ac828e20282270bd921784d7b1e796760e40442
Darshan <nepaleseboyooo@gmail.com>
2021-06-19 20:34:25 +0545

    added user model

---
+++ backend/accounts/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.forms import fields
+
+class ExtendedUserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+    
+    class Meta:
+        model = User
+        fields = ('username', 'emali', 'password1', 'password2')
+
+    def save(self, commit=True):
+        user = super().save(commit=False) 
+        user.email = self.cleaned_data['email']
+        
+        if commit:
+            user.save():
+        return user
+
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ('name', 'batch', 'roll_no','address')
--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -4,6 +4,5 @@ from . import views
 urlpatterns = [
     
     path('register/',views.register, name='user-register' ),
-    path('user_login/',views.user_login, name='user-_ogin' ),
-    path('staff_login/',views.staff_login, name='staff_login' ),
+    
 ]
