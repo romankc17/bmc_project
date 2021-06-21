9312726e97aea513b4b05ef300529724d3cb1130
Your Name <romanchhetri02@gmail.com>
2021-06-21 19:05:29 +0545

    my branch

--- backend/college/apps.py
+++ backend/college/apps.py
@@ -3,6 +3,5 @@ from django.apps import AppConfig
 
 class CollegeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'college'
-    asdfasdfasf
-asdfasdfasfd
+    name = 'samir'
+
