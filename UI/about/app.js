ca55f4d39e81b46ff0133189eb373a8b453a6cca
Anjana Poudel <anzana000@gmail.com>
2021-07-05 12:58:00 +0545

    responsive task remained

--- UI/about/app.js
+++ UI/about/app.js
@@ -1,66 +1,61 @@
 const membersInfo = [
     {
-        name: "Prakash BL Dhakal",
-        position: "President",
-        img: "./imgs/Prakash-removebg-preview.png"
-    },
-    {
         name: "Mahendra Kandel",
         position: "Vice President",
-        img: "./imgs/Prakash-removebg-preview.png"
+        img: "./imgs/next.png"
     },
     
     {
         name: "Kiran Kafle",
         position: "Secretary",
-        img: "./imgs/Keron-removebg-preview.png"
+        img: "./imgs/next.png"
     },
     {
         name: "Rabin Pandey",
         position: "Joint Secretary",
-        img: "./imgs/Prakash-removebg-preview.png"
+        img: "./imgs/next.png"
     },
    
     {
         name: "Manoj Pokharel",
         position: "Event Manager",
-        img: "./imgs/Prakash-removebg-preview.png"
+        img: "./imgs/next.png"
     },
     {
         name: "Aashish Sharma",
         position: "Graphics Designer",
-        img: "./imgs/Prakash-removebg-preview.png"
+        img: "./imgs/next.png"
     },
     {
         name: "Ganga Pandey",
         position: "Executive Member",
-        img: "./imgs/Prakash-removebg-preview.png"
+        img: "./imgs/clipart55403.png"
     },
     {
         name: "Bhisma Chapagain",
         position: "Executive Member",
-        img: "./imgs/Prakash-removebg-preview.png"
+        img: "./imgs/clipart55403.png"
     },
     {
         name: "Santosh Paudel",
         position: "Executive Member",
-        img: "./imgs/Prakash-removebg-preview.png"
+        img: "./imgs/next.png"
     },
    
     {
         name: "Sushant Banjade",
         position: "Executive Member",
-        img: "./imgs/Prakash-removebg-preview.png"
+        img: "./imgs/next.png"
     },
     {
         name: "Jaya Pangeni",
         position: "Executive Member",
-        img: "./imgs/Prakash-removebg-preview.png"
+        img: "./imgs/clipart55403.png"
     },
     {
         name: "Ganesh Gyawali",
         position: "Executive Member",
-        img: "./imgs/Prakash-removebg-preview.png"
+        img: "./imgs/next.png"
     }
 ]
 
@@ -73,10 +68,10 @@ const ourTeam = membersInfo.map(member => {
     <h2 class ="mt-5">${name}</h2>
     <p>${position}</p>
     <div class = "social-icons">
-    <a href = ""> <i class="fab fa-facebook"></i></a>
+    <a href = ""><i class="fab fa-facebook"></i></a>
     <a href = ""><i class="fab fa-instagram"></i></a>
-    <a href = ""> <i class="fab fa-github"></i></a>
-    <a href = ""> <i class="fab fa-linkedin-in"></i></a>
+    <a href = ""><i class="fab fa-github"></i></a>
+    <a href = ""><i class="fab fa-linkedin-in"></i></a>
     </div>
     </div>`
 });
