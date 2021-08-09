b5c1e8808d2cf376db993cd681b27a1b7e9f5679
shrish karki <shrishkarki6669@gmail.com>
2021-08-09 20:58:16 +0545

    END

--- frontend/Landingpage/Template/indexTabs.js
+++ frontend/Landingpage/Template/indexTabs.js
@@ -18,24 +18,4 @@ function openCity(evt, cityName) {
   document.getElementById("defaultOpen").click();
 
 
-  // GO TO TOP ICON
-
-
-const topIcon=document.querySelector('.gotoTop');
- topIcon.addEventListener('click',()=>{
-  window.scrollTo({
-    top:0,
-    left:0,
-    behavior:"smooth"
-  });
- })
-
-window.addEventListener("scroll",()=>{
-  if (document.body.scrollTop > 800 || document.documentElement.scrollTop > 800) {
-   topIcon.classList.add("show");
-  }
- else{
-   topIcon.classList.remove("show");
- }
-})
  
