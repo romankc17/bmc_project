b9449cccdfa434196d7a5025aee23e32300de7d1
romankc17 <romanchhetri02@gmail.com>
2021-08-10 17:24:13 +0545

    latest events in index

--- backend/bmc/views.py
+++ backend/bmc/views.py
@@ -1,6 +1,12 @@
 from django.contrib import messages
 from django.shortcuts import render
 
+from association.models import Event
+
 
 def home(request):
-    return render(request, 'index.html')
+    events = Event.objects.order_by('-date')[:2]
+    context = {
+        'events': events,
+    }   
+    return render(request, 'index.html', context)
