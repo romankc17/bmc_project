27914ceadb4055ac1f8d8c7cfe5372e35e5650e3
romankc17 <romanchhetri02@gmail.com>
2021-08-12 10:08:23 +0545

    removed heroku

--- backend/bmc/settings.py
+++ backend/bmc/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 import os.path
-import django_heroku
+# import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
